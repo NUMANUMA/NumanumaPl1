ec596ab4e203d426465fd0e4eb89e114bcff7404
NUMANUMA <nazottewho@yahoo.co.jp>
2013-10-14 19:37:59 +0900

    NumanumaPl1 add

---
+++ src/com/github/NUMANUMA/NumanumaPl1/NumanumaPl1.java
@@ -0,0 +1,17 @@
+package com.github.NUMANUMA.NumanumaPl1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NumanumaPl1 extends JavaPlugin
+{
+
+    public void onEnable()
+    {
+
+    }
+
+    public void onDisable()
+    {
+
+    }
+}
