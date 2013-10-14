9890fe357dce7b702a1662a81b03e6f3d23d1ca5
NUMANUMA <nazottewho@yahoo.co.jp>
2013-10-14 19:47:59 +0900

    add-1
    NumanumaPl1 edit
    Event add

---
+++ src/com/github/NUMANUMA/NumanumaPl1/Event.java
@@ -0,0 +1,9 @@
+package com.github.NUMANUMA.NumanumaPl1;
+
+import org.bukkit.event.Listener;
+
+public class Event implements Listener
+{
+    public Event(NumanumaPl1 plugin){
+    }
+}
--- src/com/github/NUMANUMA/NumanumaPl1/NumanumaPl1.java
+++ src/com/github/NUMANUMA/NumanumaPl1/NumanumaPl1.java
@@ -1,17 +1,23 @@
 package com.github.NUMANUMA.NumanumaPl1;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NumanumaPl1 extends JavaPlugin
-{
+public class NumanumaPl1 extends JavaPlugin {
+    Logger log;
+    public Event eve = new Event(this);
 
-    public void onEnable()
-    {
+    @Override
+    public void onEnable() {
+        log = this.getLogger();
+        log.info("プラグインが有効になりました。");
 
+        getServer().getPluginManager().registerEvents(eve, this);
     }
 
-    public void onDisable()
-    {
-
+    @Override
+    public void onDisable() {
+        log.info("プラグインが無効になりました。");
     }
 }
